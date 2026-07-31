9165725e37800ddb51f55a861fb70545923ef224
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-07-31 15:19:22 +0200

    Enhance SingleEmployee component layout; add employee details table and improve styling

--- src/pages/SingleEmployee.jsx
+++ src/pages/SingleEmployee.jsx
@@ -1,9 +1,9 @@
-import { useNavigate, useParams } from "react-router"
+import { useNavigate, useParams, Link} from "react-router"
 import { useEffect, useState } from "react"
 import axios from "axios"
 
 export default function SingleEmployee() {
-    const {id} = useParams()
+    const { id } = useParams()
 
     const [employee, setEmployee] = useState(null);
     const [loading, setLoading] = useState(true)
@@ -12,7 +12,7 @@ export default function SingleEmployee() {
         axios.get(`http://localhost:8000/api/employees/${id}`)
             .then(res => {
                 console.log(res.data),
-                setEmployee(res.data)
+                    setEmployee(res.data)
             })
             .catch(err => console.error(err))
             .finally(() => setLoading(false))
@@ -29,9 +29,53 @@ export default function SingleEmployee() {
 
 
     return (
-        <div>
-            <h1>Single Employee Page</h1>
-            <img src={employee?.image} alt={employee?.name} />
+        <div className=" m-4">
+
+            <div className="d-flex justify-content-around align-items-center mb-4">
+                <img src={employee.image} alt={employee.name} className="d-none d-md-block w-25 h-25 rounded-circle" />
+                <div className="d-flex flex-column">
+                    <h5 className="text-center">Ruolo Dipendente</h5>
+                    <p className="text-center p-2 border bg-primary text-white rounded">{employee.job_title}</p>
+                    <h5 className="text-center">Competenze:</h5>
+                    <ul className="list-unstyled">
+                        {employee.skills?.map(skill => (
+                            <li key={skill.id} className="p-2 m-2 rounded " style={{ backgroundColor: skill.color, border: '1px solid #dee2e6' }}>
+                                {skill.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+            </div>
+
+
+
+
+
+
+            <table className="table table-striped-columns">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Cognome</th>
+                        <th scope="col">Dipartimento</th>
+                        <th scope="col">Mail</th>
+                        <th scope="col">Telefono</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{employee.name}</td>
+                        <td>{employee.last_name}</td>
+                        <td><Link className="text-decoration-none text-dark" to={`/dipartimenti/${employee.department?.id}`}>{employee.department?.name}</Link></td>
+                        <td>{employee.email}</td>
+                        <td>{employee.phone_number}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+
         </div>
     );
 }
